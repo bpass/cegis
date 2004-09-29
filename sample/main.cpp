0ec818929fea258f4a15e97da9d39b169808be03
jtrent <jtrent>
2004-09-29 15:50:05 +0000

    My update with windows instructions

--- sample/main.cpp
+++ sample/main.cpp
@@ -1,8 +1,43 @@
 #include <iostream.h>
 
+
+
+/*	This program is very simple. Now that you (should) have compiled and run it
+	please do the following (see me (Jason) if you have any troubles):
+
+
+	1) Add your name to the list in the cout statement below
+
+	2) Compile and run the program to make sure that it works as expected
+
+	Windows Users:
+		3) Go to where you have main.cpp stored, right-click on it and 
+			choose "CVS Diff..." 
+
+		4) When prompted for an external diff program, you can select WinDiff.
+			This is usually found in C:\Program Files\Microsoft Visual Studio\Common\Tools\
+
+		5) Review the diff and make sure that what it displays makes "sense"
+
+		6) Right-click on the FOLDER containing main.cpp and choose "CVS Update"
+		   This will get the newest version of all files in the project and merge them
+		   with yours
+
+		7) Right-click on the FOLDER containing main.cpp and choose "CVS Commit..."
+		   This will commit your file changes to the repository
+
+        8) When the checkin dialog appears enter a comment and click ok
+
+
+
+
+
+
+
+
 int main()
 {
-   cout << "Demo program works!\n";
+   cout << "CVS Demo\n";
    cout << "\nOthers who have checked out:\n"
         << "\tJason Trent\n";
 

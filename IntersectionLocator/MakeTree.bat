d0ebbb3c25c08a4eb0dc0c8424abd08de2eea32a
rstelzleni <rstelzleni>
2004-10-18 22:56:07 +0000

    Added $Id$ tags and some pseudo-explanatory comments.

--- IntersectionLocator/MakeTree.bat
+++ IntersectionLocator/MakeTree.bat
@@ -1,5 +1,18 @@
 @echo off
 
+rem This bat file creates a directory tree for creating output from
+rem the intersection locator's current driver.  It is really just to
+rem save me time more than anything.  Note that the clean option is
+rem very dangerous.  Make sure you are doing what you intend to.
+rem
+rem Programmer(If it can be called that?): Ryan Stelzleni
+rem Date: 10/18/04
+
+rem Some kind of CVS incantation here
+rem $Id: MakeTree.bat,v 1.2 2004/10/18 22:56:07 rstelzleni Exp $
+
+
+
 if %1 == %nothing% goto end
 if %1 == clean goto clean
 

30cbfe1197e2171c35d2106d386d3dd18fa6e652
rstelzleni <rstelzleni>
2004-11-19 17:51:53 +0000

    Added some notes about the transformation problems and about the classifier.  Also added some suggestions on how to improve the process.

--- IntersectionLocator/doc/doxygenFiles/doxygenMain.h
+++ IntersectionLocator/doc/doxygenFiles/doxygenMain.h
@@ -1,18 +1,18 @@
 
 // CVS magic symbols for voodoo practitioners
-// $Id: doxygenMain.h,v 1.8 2004/11/18 20:11:39 rstelzleni Exp $
+// $Id: doxygenMain.h,v 1.9 2004/11/19 17:51:53 rstelzleni Exp $
 
 /** \mainpage IntersectionLocator
  *
  * \section intro Introduction
- * <P>
+ * 
  * The intersection locator is an attempt to automatically correct vector
  * road maps without human intervention.  The basic idea is to use a raster
  * image, like an aerial photograph of an area, to locate the true
  * locations of the intersections, and then to use a rubber sheeting 
  * transformation with the newly identified better intersection locations
  * as the control points to correct the vector roads.  
- * </P><P>
+ * 
  * The new intersection locations are found using a pattern matching 
  * technique.  First the locations of the intersection in the vector image
  * are found in the raster image.  Next the vector image is used to create
@@ -23,10 +23,10 @@
  * that is near the intersection in the vector data and that matches the
  * temporary raster image best.  This spot is then used as the new 
  * better location for the intersection.
- * </P><P>
+ * 
  * That is a very brief overview of the process that skims over the 
  * road pixel identification, and control point filtering.  
- * </P>
+ * 
  *
  * \section usage Usage
  * \ref preparingForNewData Instructions for preparing for a new dataset <BR>
@@ -42,13 +42,13 @@
  * sorry.
  *
  * \section installation Installation
- * <P>
+ * 
  * This project was developed in VC6.  I haven't tried to build it in
  * any other environments, but in theory I didn't use anything that
  * isn't portable.  To build the project you will need the GDAL/OGR 
  * library, which is available at 
  * <a href="http://www.remotesensing.org/gdal/">www.remotesensing.org/gdal</a>
- * </P><P>
+ * 
  * I built the project using version 1.2.0 of that library.  If you
  * download the pre-built gdal-120-ntbin.zip package it includes 
  * everything you will need.  If you can't find that package then feel
@@ -56,7 +56,7 @@
  * there are no guarantees.  If whatever version of gdal you get doesn't
  * include the proj dynamic library then you will have to find a copy
  * of that too.
- * </P><P>
+ * 
  * To build the project the include files for gdal will need to be in
  * your include path, and you will need to link against the static library
  * gdal_i.lib.  The easiest way to handle that in VC6 is to add it to the
@@ -65,18 +65,19 @@
  * website mentioned above, in the mentined package for instance)
  * somewhere where the program can find them.  In the project directory 
  * is a simple solution.
- * </P><P>
+ * 
  * Running the program is a whole 'nother story.  Because the requirements
  * have been changing so quickly my solution has been to write a specialized
  * driver for each run.  To do this you will need to do some reading up
  * on how the program works, and that's what this documentation is for.
  * Good Luck.
- * </P>
+ * 
  *
  * \subsection linuxInstallation Installation in Linux
  * \ref buildingOnLinux Linux Installation Guide
  *
  * \section mainPageNotes Notes
+ * \subsection dependencyOnGDALOGR Dependency on GDAL/OGR
  * One thing that I think it is good to point out is why there is a 
  * dependency on the GDAL/OGR library.  The reason is that parsing 
  * geospatial data files, like Arc Coverages and Shapefiles, isn't a
@@ -95,4 +96,45 @@
  * I definitely should have encapsulated that library instead of using
  * it directly, but hindsight is 20/20 and I'm pretty sure I'm blind
  * when it comes to foresight.  
+ *
+ * \subsection transformationProblems Transformation Problems
+ * The rubber sheeting transformation that the program currently uses
+ * has some major problems that could be improved upon.  The transformation
+ * works by creating a set of traingles (a triangulation) that includes
+ * every control point as a triangle vertex.  Then it creates a set of
+ * linear transformations, one for each triangle, that move the original
+ * points onto the control points.  Once this transformation is defined
+ * the program looks to see what triangle each point is in and then applies
+ * the linear transformation for that triangle to the point.  
+ *
+ * This works fine so long as all points are contained in the triangulation,
+ * and so long as there are as few small angles as possible in the 
+ * triangles that are generated.  The few small angles problem is helped
+ * by the basic properties of the Delauney Triangulation (it maximizes
+ * the minimum angle in the set of triangles), but there still wind up
+ * being some pretty small angles near the edges of the area that is
+ * covered by the control points.  Knuth describes a method of performing
+ * this triangulation which assumes that we have three imaginary control
+ * points at infinity, and thus fixes the problem of small edges at 
+ * the outside borders, but I have had trouble finding a paper describing
+ * his method.  
+ *
+ * The other problem with the triangulation occurs when the control points
+ * create a set of triangles that doesn't contain every original point.
+ * This results in some lines where some of the control points are moved
+ * and others are not, and so creates an ugly, wavy line.  Knuth's method
+ * would also correct this problem, so a better triangulation would be
+ * a major source of improvement.
+ *
+ * \subsection classification Classification
+ * The classifier could be improved a great deal, but it will not be easy
+ * to do.  The current classifier uses the three attributes, hue, saturation,
+ * and value of a color to tell if it is a road.  One possible alternative
+ * would be to use the three as a single attribute of the pixel, instead
+ * of as three separate attributes.  Another way that the classifier might
+ * be able to be improved would be to add more attributes to the list of
+ * what is checked.  i.e. proximity to other road pixels, or road pixels 
+ * are more likely to form linear features, etc.  I'm not sure how you
+ * would implement those in such a way that the classifier could use the
+ * information, but if you could I imagine that it would help.
  */

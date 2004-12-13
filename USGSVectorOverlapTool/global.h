e1fbff29f56c5cdd00250ca249deee7bf2882657
rstelzleni <rstelzleni>
2004-12-13 16:46:31 +0000

    This file is intended to hold typedefs and defines that will be used by all or most of the files in this project.

---
+++ USGSVectorOverlapTool/global.h
@@ -0,0 +1,17 @@
+/**
+ * @file global.h
+ * This file is just to contain definitions that are needed everywhere.
+ *
+ * $Id: global.h,v 1.1 2004/12/13 16:46:31 rstelzleni Exp $
+ */
+
+
+
+typedef std::list<Feature *>::iterator   FeatureIterator;
+typedef std::list<Feature *>             FeatureList;
+typedef std::list<FeatureIterator>       FeatureIteratorList;
+typedef std::vector<FeatureIteratorList> FeatureGridVector;
+
+
+
+

c247d59a0d797a493a9f898380971cdf4c60ca25
jtrent <jtrent>
2004-12-14 19:00:37 +0000

    still doesn't work in windows. I have made teh code look a little prettier and I removed acForm.cpp and acForm.h since they are genereated at compile time and shouldn't have been added to teh project. The current version produces 11.8B on teh population data sum in Moll 30arc. We ae getting ready to try using weighted sums in the resample to see if that helps.

--- MapIMG_1.02_resample/getprojinfo.h
+++ MapIMG_1.02_resample/getprojinfo.h
@@ -124,7 +124,6 @@ bool mapimg(const char * mapimginfilename, const char * mapimgoutfilename,
     FILE *paramfile = fopen( logFile, "wa");
 
 
-    /* jtrent resample */
 
     for(out_line = 0; out_line < outimg.nl; out_line++) 		// For each output image line
     {
@@ -146,260 +145,26 @@ bool mapimg(const char * mapimginfilename, const char * mapimgoutfilename,
 		out[0] = outimg.ul_x + (out_samp * outimg.pixsize);	// Calc out-img proj_X coord
 
 
+		find2corners = 0;	// must get all 4 corners at beg. of line
 
+		double inbox[5][2];
 
+		for( int inbox_i = 0; inbox_i < 5; inbox_i++ )
+			for( int inbox_j = 0; inbox_j < 2; inbox_j++ )
+				inbox[inbox_i][inbox_j] = 0;
 
-/********** resample jtrent *******************/
+		find2corners = 0;
 
-                            find2corners = 0;	// must get all 4 corners at beg. of line
-
-			    double inbox[5][2];
-
-			    for( int inbox_i = 0; inbox_i < 5; inbox_i++ )
-			         for( int inbox_j = 0; inbox_j < 2; inbox_j++ )
-			              inbox[inbox_i][inbox_j] = 0;
-
-                            find2corners = 0;
-
-                            if( get_coords( outimg, inimg, out, inbox, out_line, out_samp, paramfile ) )
-                            {
-//                            	for( int indexii = 0; indexii < 5; indexii++ )
-//                            	{
-//                            		printf( "\t ( %f, %f )\n", inbox[indexii][0], inbox[indexii][1] );
-//                            	}
-
-                              in_line = ((inimg.ul_y - in[1]) / inimg.pixsize) + 0.5;
-                              in_samp = ((in[0] - inimg.ul_x) / inimg.pixsize) + 0.5;
-
-			        // Still inside the input image??
-  			        // ------------------------------
-             			if (in_line<0||in_samp<0||in_line>=inimg.nl||in_samp>=inimg.ns)
-                		{
-			           *( (type*)mapimgoutbuf + out_samp) = fill; // Barely ever happans, if at all
-			        }
-
-			        // Assign the appropriate input image pixel to the output image using
-           			// nearest-neighbor resampling. (...the half-pixel rounding is done in
-	            		// the in_line, in_samp calculate above...)
-	              		// ---------------------------------------------
-		        	else
-			        {
-                                  //----- compute minbox -----//
-	                          int maxx = 0,
-                                      maxy = 0,
-                                      minx = inimg.ns,
-                                      miny = inimg.ns;
-
-	                          for(int inbox_index = 0; inbox_index < 4; ++inbox_index)
-	                          {
-	                                if(maxx < inbox[inbox_index][0])
-		                           maxx = inbox[inbox_index][0];
-	                                else
-	                                {
-	                                   if(minx > inbox[inbox_index][0])
-		                              minx = inbox[inbox_index][0];
-	                                }
-
-	                                if(maxy < inbox[inbox_index][1])
-		                           maxy = inbox[inbox_index][1];
-	                               // else
-	                               // {
-	                                    if(miny > inbox[inbox_index][1])
-		                               miny = inbox[inbox_index][1];
-	                                //}
-	                            } //for inbox_index
-
-                                //----- check each pixel in minbox & compile stats -----//
-	                        int boxError = 1;
-	                        int yCoverageArea = iround( maxy ) - iround( miny ) + 1;
-	                        int xCoverageArea = iround( maxx ) - iround( minx ) + 1;
-
-//	                        long yCoverageArea = ((long)(maxy+0.5) - (long)(miny+0.5));
-//                              long xCoverageArea = ((long)(maxx+0.5) - (long)(minx+0.5));
-
-                                if( yCoverageArea < 1 ) yCoverageArea = 1;
-                                if( yCoverageArea == 1 && maxy != miny ) yCoverageArea++;
-
-                                if( xCoverageArea < 1 ) xCoverageArea = 1;
-                                if( xCoverageArea == 1 && maxx != minx ) xCoverageArea++;
-
-	                        int coverageSize =  (int)( yCoverageArea * xCoverageArea );
-
-//                                printf( "minx = %i\tmaxx = %i\tminy = %i\tmaxy = %i\n", minx, maxx, miny, maxy );
-//	                        printf( "coverageSize = %i\n", coverageSize );
-//	                        fflush( stdout );
-
-                                void* inputCoverage = (void*)malloc( sizeof(type) * coverageSize+1 );
-                                int offset = 0;
-                                double coord[2] = { 0 };
-
-/*
-                                printf( "Computing box centered @ ( %f , %f )\n", inbox[4][0], inbox[4][1] );
-                                printf( "Y length is from %i to  %i ..... %f %f %f %f\n", miny, maxy,
-                                        inbox[0][1],
-                                        inbox[1][1],
-                                        inbox[2][1],
-                                        inbox[3][1] );
-                                fflush( stdout );
-  */
-
-                                offset = 0;
-
-	                        for(int currentY = iround(miny); currentY <= iround(maxy); currentY++)
-	                        {
-                                    coord[1] = currentY;
-
-                                    //Loads into memory the current line of input needed
-                                    get_line(  mapimginbuf, coord[1]*inimg.ns, inimg.ns, useType );
-				    if( mapimginbuf == NULL )
-				    	break;
-				    
-	                            for(int currentX = iround(minx); currentX <= iround(maxx); currentX++)
-	                            {
-	                                coord[0] = currentX;
-	                                if( inBox( inbox, coord ) )
-	                                {
-                                 	      boxError = 0;
-
-                                                *( (type*)inputCoverage + offset) = *(((type*)mapimginbuf + (int)coord[0]));
-	                                }//if inBox
-	                                else
-	                                {
-                                              *( (type*)inputCoverage + offset) = 0;
-                                        }
-
-//                                        printf( "offset = %i    ... coverageSize = %i\n", offset, coverageSize );
-//                                        fflush( stdout );
-
-                                        offset++;
-	                            }// for curx
-	                        }//for cury
-
-
-
-//                                 printf( "finished loop\n" );
-//                                 fflush( stdout );
-                                //----- finish statistical analysis -----//
-                                if(boxError)	//no pixels from rectangle in the minbox, get NN.
-	                        {
-                                   //Loads into memory the current line of input needed
-                                   get_line(  mapimginbuf, inbox[4][1]*inimg.ns, inimg.ns+1, useType );
-
-//                                   printf( "boxError doing nearest neighbor\n" );
-//                                   fflush( stdout );
-                                   //references specific element of input
-//                                   *( (type*)mapimgoutbuf + out_samp) = 2;  //Purple 4228 edges
-//                                   *( (type*)mapimgoutbuf + out_samp) = fill;
-                                   *( (type*)mapimgoutbuf + out_samp) = *(((type*)mapimginbuf + (int)(inbox[4][0])));
-                                }
-	                        else	//!boxError
-	                        {
-	                           type sumValue = (type)0;
-
-//                                   printf( "Success doing sum\n" );
-//                                   fflush( stdout );
-
-
-	                           for( int coverageIndex = 0; coverageIndex < coverageSize; coverageIndex++ )
-	                           {
-                                       sumValue += *( (type*)inputCoverage + coverageIndex );
-	                           }
-//	                           *( (type*)bobBuffer + out_samp) = *( (type*)inputCoverage + coverageIndex);
-	                           *( (type*)mapimgoutbuf + out_samp) = sumValue;
-//	                           *( (type*)mapimgoutbuf + out_samp) = 2; //dark green most of image  36654
-
-//                                   printf( "finished sum = %u\n", (unsigned int)sumValue );
-//                               	   fflush( stdout );
-			        }
-
-			        free( inputCoverage );
-                              }
-                            }
-                            else
-                            {
-//                            	printf( "get_coords returned zero\n" );
-//                           	fflush( stdout );
-   		                *( (type*)mapimgoutbuf + out_samp) = fill; //light gray 23918 fill
-                            }
-
-/********** resample jtrent *******************/
-/*
-		// Goode's already does error checking; skip the wrap-around check
-		// ---------------------------------------------------------------
-		if(outimg.sys == 24)
-		{
-//			gctp(out,&outimg.sys,&outimg.zone,outimg.pparm,&outimg.unit,&outimg.datum,
-//				&zero,"",&zero,"",in,&inimg.sys,&inimg.zone,inimg.pparm,&inimg.unit,
-//				&inimg.datum,"","",&status);
-			gctp(out,&outimg.sys,&outimg.zone,outimg.pparm,&outimg.unit,&outimg.datum,
-				&errorMode,logFile, &paramMode,logFile,paramfile,in,&inimg.sys,&inimg.zone,inimg.pparm,&inimg.unit,
-				&inimg.datum,"","",&status);
-
-		}
-
-		else
-		{
-
-			// Check for a wrap-around condition by mapping Proj --> Lat/Long and
-			// then back again.  Is it the same coordinate?  If not, consider the
-			// pixel in the wrap-around area...
-			// --------------------------------
-/*			gctp(out,&outimg.sys,&outimg.zone,outimg.pparm,&outimg.unit,&outimg.datum,
-				&zero,"",&zero,"",t1,&zero,&outimg.zone,pparm,&four,
-				&outimg.datum,"","",&status);
-
-			gctp(t1,&zero,&outimg.zone,pparm,&four,&outimg.datum,
-				&zero,"",&zero,"",t2,&outimg.sys,&outimg.zone,outimg.pparm,&outimg.unit,
-				&outimg.datum,"","",&status);
-
-*/
-/*			gctp(out,&outimg.sys,&outimg.zone,outimg.pparm,&outimg.unit,&outimg.datum,
-				&errorMode,logFile,&paramMode,logFile,paramfile,t1,&zero,&outimg.zone,pparm,&four,
-				&outimg.datum,"","",&status);
-
-			gctp(t1,&zero,&outimg.zone,pparm,&four,&outimg.datum,
-				&errorMode,logFile,&paramMode,logFile,paramfile,t2,&outimg.sys,&outimg.zone,outimg.pparm,&outimg.unit,
-				&outimg.datum,"","",&status);
-
-			temp1=fabs(t2[0]-out[0]);
-			temp2=fabs(t2[1]-out[1]);
-
-			if(temp1 > (outimg.pixsize/2) || temp2 > (outimg.pixsize/2))
-			{
-				status = NOT_OK;
-			}
-
-			else
-			{
-				// Now, if still OK, do the projection transformation
-				// --------------------------------------------------
-//				gctp(out,&outimg.sys,&outimg.zone,outimg.pparm,&outimg.unit,&outimg.datum,
-//					&zero,"",&zero,"",in,&inimg.sys,&inimg.zone,inimg.pparm,&inimg.unit,
-//							&inimg.datum,"","",&status);
-				gctp(out,&outimg.sys,&outimg.zone,outimg.pparm,&outimg.unit,&outimg.datum,
-				     &errorMode,logFile,&paramMode,logFile,paramfile,in,&inimg.sys,&inimg.zone,inimg.pparm,&inimg.unit,
-							&inimg.datum,"","",&status);
-			}
-		}
-
-		if(status != OK)
-		{
-                   *( (type*)mapimgoutbuf+out_samp) = fill;	// Fill if not valid...
-		}
-
-		// Calculate the input image coordinate (line, sample) from the projection
-		// coordinate that resulted from the output-to-input projection transformation
-		// ---------------------------------------------------------------------------
-		else
+		if( get_coords( outimg, inimg, out, inbox, out_line, out_samp, paramfile ) )
 		{
 			in_line = ((inimg.ul_y - in[1]) / inimg.pixsize) + 0.5;
 			in_samp = ((in[0] - inimg.ul_x) / inimg.pixsize) + 0.5;
-
+	
 			// Still inside the input image??
-  			// ------------------------------
+			// ------------------------------
 			if (in_line<0||in_samp<0||in_line>=inimg.nl||in_samp>=inimg.ns)
 			{
-			    *( (type*)mapimgoutbuf + out_samp) = fill;
+				*( (type*)mapimgoutbuf + out_samp) = fill; // Barely ever happans, if at all
 			}
 
 			// Assign the appropriate input image pixel to the output image using
@@ -408,15 +173,106 @@ bool mapimg(const char * mapimginfilename, const char * mapimgoutfilename,
 			// ---------------------------------------------
 			else
 			{
+				//----- compute minbox -----//
+				int maxx = 0,
+				maxy = 0,
+				minx = inimg.ns,
+				miny = inimg.ns;
+		
+				for(int inbox_index = 0; inbox_index < 4; ++inbox_index)
+				{
+					if(maxx < inbox[inbox_index][0])
+						maxx = inbox[inbox_index][0];
+				else
+				{
+					if(minx > inbox[inbox_index][0])
+						minx = inbox[inbox_index][0];
+				}
+		
+				if(maxy < inbox[inbox_index][1])
+					maxy = inbox[inbox_index][1];
+				// else
+				// {
+					if(miny > inbox[inbox_index][1])
+					miny = inbox[inbox_index][1];
+				//}
+				} //for inbox_index
 
-                            //Loads into memory the current line of input needed
-                            get_line(  mapimginbuf, in_line*inimg.ns, inimg.ns+1, useType );
+				//----- check each pixel in minbox & compile stats -----//
+				int boxError = 1;
+				int yCoverageArea = iround( maxy ) - iround( miny ) + 1;
+				int xCoverageArea = iround( maxx ) - iround( minx ) + 1;
+		
+				if( yCoverageArea < 1 ) yCoverageArea = 1;
+				if( yCoverageArea == 1 && maxy != miny ) yCoverageArea++;
+		
+				if( xCoverageArea < 1 ) xCoverageArea = 1;
+				if( xCoverageArea == 1 && maxx != minx ) xCoverageArea++;
+		
+				int coverageSize =  (int)( yCoverageArea * xCoverageArea );
+		
+				void* inputCoverage = (void*)malloc( sizeof(type) * coverageSize+1 );
+				int offset = 0;
+				double coord[2] = { 0 };
+		
+		
+				offset = 0;
+		
+				for(int currentY = iround(miny); currentY <= iround(maxy); currentY++)
+				{
+					coord[1] = currentY;
+		
+					//Loads into memory the current line of input needed
+					get_line(  mapimginbuf, coord[1]*inimg.ns, inimg.ns, useType );
+					if( mapimginbuf == NULL )
+					break;
+					
+					for(int currentX = iround(minx); currentX <= iround(maxx); currentX++)
+					{
+					coord[0] = currentX;
+					if( inBox( inbox, coord ) )
+					{
+						boxError = 0;
+		
+						*( (type*)inputCoverage + offset) = *(((type*)mapimginbuf + (int)coord[0]));
+					}//if inBox
+					else
+					{
+						*( (type*)inputCoverage + offset) = 0;
+					}
+		
+					offset++;
+					}// for curx
+				}//for cury
+		
+				//----- finish statistical analysis -----//
+				if(boxError)	//no pixels from rectangle in the minbox, get NN.
+				{
+					//Loads into memory the current line of input needed
+					get_line(  mapimginbuf, inbox[4][1]*inimg.ns, inimg.ns+1, useType );
+		
+					*( (type*)mapimgoutbuf + out_samp) = *(((type*)mapimginbuf + (int)(inbox[4][0])));
+				}
+				else	//!boxError
+				{
+					type sumValue = (type)0;
+		
+					for( int coverageIndex = 0; coverageIndex < coverageSize; coverageIndex++ )
+					{
+						if( *( (type*)inputCoverage + coverageIndex ) != -9999 )
+							sumValue += *( (type*)inputCoverage + coverageIndex );
+					}
+					*( (type*)mapimgoutbuf + out_samp) = sumValue;
+				}
 
-                            //references specific element of input
-			    *( (type*)mapimgoutbuf + out_samp) = *((type*)mapimginbuf + in_samp);
+				free( inputCoverage );
 			}
 		}
-		*/
+		else
+		{
+			*( (type*)mapimgoutbuf + out_samp) = fill; 
+		}
+
 	}
 
 

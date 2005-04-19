da744753dc55fe13dc9ae1975b4866cca094a253
ahartman <ahartman>
2005-04-19 21:50:27 +0000

    QuadraticFormula now compiles.  Had to remove an exception that was to be thrown whenever the degree of the polynomial was invalid; this should be corrected in the future.

--- PolynomialRootFinder/QuadraticFormula.h
+++ PolynomialRootFinder/QuadraticFormula.h
@@ -1,11 +1,20 @@
+/**
+ * @file QuadraticFormula.h
+ * @author Austin Hartman
+ * $Id: QuadraticFormula.h,v 1.2 2005/04/19 21:50:27 ahartman Exp $
+ */
+
 #ifndef AUSTIN_QUADRATICFORMULA_H
 #define AUSTIN_QUADRATICFORMULA_H
+
+#include <exception>
 
 #include "Polynomial.h"
 
 template<class T>
 class QuadraticFormula
 {
+public:
     typedef std::vector<T> Roots;
     Roots operator()(const Polynomial<T>& p) const;
 
--- PolynomialRootFinder/QuadraticFormula.hpp
+++ PolynomialRootFinder/QuadraticFormula.hpp
@@ -1,27 +1,34 @@
+/**
+ * @file QuadraticFormula.hpp
+ * @author Austin Hartman
+ * $Id: QuadraticFormula.hpp,v 1.2 2005/04/19 21:50:27 ahartman Exp $
+ */
+
 template<class T>
 typename QuadraticFormula<T>::Roots
 QuadraticFormula<T>::operator()(const Polynomial<T>& p) const
 {
-    if(p.degree() != 2)
-    {
-        throw typename QuadraticFormula<T>::InvalidPolynomial;
-    }
-
+//    if(p.degree() != 2)
+//    {
+//        throw QuadraticFormula<T>::InvalidPolynomial;
+//    }
+//
     typename QuadraticFormula<T>::Roots roots;
 
     const T& a = p.getCoefficient(2);
     const T& b = p.getCoefficient(1);
     const T& c = p.getCoefficient(0);
 
-    const T discriminant = b*b - 4*a*c;
+    const T discriminant = b*b - 4*a*c;
 
-    // if the discriminant is less than 0, return a blank solution
-    // because there are no real roots
     if(discriminant > 0)
     {
-        // calculate the two roots
-        const T sqrtDiscriminant = std::sqrt(discriminant)
-        roots.push_back( T(2*c / (-b + sqrtDiscriminant)) );
+        // calculate the two roots or the single root (if a == 0)
+        const T sqrtDiscriminant = std::sqrt(discriminant);
+        if(a != 0)
+        {
+            roots.push_back( T(2*c / (-b + sqrtDiscriminant)) );
+        }
         roots.push_back( T(2*c / (-b - sqrtDiscriminant)) );
     }
     else if(discriminant == 0)

bd28b0d740623fb8c7737e7ba3d5dcc011c0c06b
ahartman <ahartman>
2005-04-22 16:57:17 +0000

    Removed some debugging output.

--- PolynomialRootFinder/PolynomialRootFinder.hpp
+++ PolynomialRootFinder/PolynomialRootFinder.hpp
@@ -1,7 +1,7 @@
 /**
  * @file PolynomialRootFinder.hpp
  * @author Austin Hartman
- * $Id: PolynomialRootFinder.hpp,v 1.2 2005/04/19 22:01:24 ahartman Exp $
+ * $Id: PolynomialRootFinder.hpp,v 1.3 2005/04/22 16:57:17 ahartman Exp $
  */
 
 template<class T>
@@ -18,28 +18,18 @@ operator()(const Polynomial<T>& p, const T& rootGuess, const T& h,
     
     while(currentP.degree() > 2)
     {
-        cerr << __FILE__ << ':' << __LINE__ 
-             << ": currentP == " << currentP << '\n';
         Muller<T> muller;
         currentRoot = muller(currentP, /*currentRoot*/rootGuess, h, epsilon, maxIterations);
         roots.push_back(currentRoot);
         currentP = currentP.deflate(currentRoot);
     }
-    cerr << __FILE__ << ':' << __LINE__ 
-         << ": roots.size() == " << roots.size() << '\n';
     
     if(currentP.degree() <= 2 && currentP.degree() >= 1)
     {
-        cerr << __FILE__ << ':' << __LINE__ 
-             << ": currentP == " << currentP << '\n';
         QuadraticFormula<T> quadf;
         typename QuadraticFormula<T>::Roots quadRoots = quadf(currentP);
-        cerr << __FILE__ << ':' << __LINE__
-             << ": quadRoots.size() == " << quadRoots.size() << '\n';
         roots.insert(roots.end(), quadRoots.begin(), quadRoots.end());
     }
-    cerr << __FILE__ << ':' << __LINE__ 
-         << ": roots.size() == " << roots.size() << '\n';
     
     return roots;
 }

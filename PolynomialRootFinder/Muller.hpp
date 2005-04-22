e1aaab60b358deb77bd400e9854c76e310d8bba4
ahartman <ahartman>
2005-04-22 15:45:21 +0000

    Added in a bunch of debugging code to see the values of all variables used by the algorithm.  It is currently commented out.

--- PolynomialRootFinder/Muller.hpp
+++ PolynomialRootFinder/Muller.hpp
@@ -1,7 +1,7 @@
 /**
  * @file Muller.hpp
  * @author Austin Hartman
- * $Id: Muller.hpp,v 1.3 2005/04/21 23:49:09 ahartman Exp $
+ * $Id: Muller.hpp,v 1.4 2005/04/22 15:45:21 ahartman Exp $
  */
 
 template<class T>
@@ -14,24 +14,45 @@ Muller<T>::operator()(const Polynomial<T>& p, const T& rootGuess, const T& h,
     T x1 = xr + h * xr;
     T x0 = xr - h * xr;
 
-    size_t iter = 0;
+//    cerr << __FILE__ << ':' << __LINE__ << '\n';
+//    cerr << "\txr = " << xr << '\n';
+//    cerr << "\tx2 = " << x2 << '\n';
+//    cerr << "\tx1 = " << x1 << '\n';
+//    cerr << "\tx0 = " << x0 << '\n';
+    
+    size_t iterations = 0;
 
     T dxr;
     do
     {
-        ++iter;
+        ++iterations;
 
-        T h0 = x1 - x0;
-        T h1 = x2 - x1;
+        const T h0 = x1 - x0;
+        const T h1 = x2 - x1;
 
-        T d0 = (p(x1) - p(x0)) / h0;
-        T d1 = (p(x2) - p(x1)) / h1;
+        const T d0 = (p(x1) - p(x0)) / h0;
+        const T d1 = (p(x2) - p(x1)) / h1;
 
-        T a = (d1 - d0) / (h1 + h0);
-        T b = a * h1 + d1;
-        T c = p(x2);
+        const T a = (d1 - d0) / (h1 + h0);
+        const T b = a * h1 + d1;
+        const T c = p(x2);
 
-        T rad = std::sqrt(b*b - 4*a*c);
+        const T radInside = b*b - 4*a*c;
+        const T rad = std::sqrt(radInside);
+
+//        cerr << __FILE__ << ':' << __LINE__ << '\n';
+//        cerr << "\th0 = " << h0 << '\n';
+//        cerr << "\th1 = " << h1 << '\n';
+//        cerr << "\td0 = " << d0 << '\n';
+//        cerr << "\t\tp(x0) = " << p(x0) << '\n';
+//        cerr << "\t\tp(x1) = " << p(x1) << '\n';
+//        cerr << "\t\tp(x2) = " << p(x2) << '\n';
+//        cerr << "\td1 = " << d1 << '\n';
+//        cerr << "\ta = " << a << '\n';
+//        cerr << "\tb = " << b << '\n';
+//        cerr << "\tc = " << c << '\n';
+//        cerr << "\tradInside = " << radInside << '\n';
+//        cerr << "\trad = " << rad << '\n';
 
         T den;
         if(b >= static_cast<T>(0))
@@ -49,10 +70,18 @@ Muller<T>::operator()(const Polynomial<T>& p, const T& rootGuess, const T& h,
         x0 = x1;
         x1 = x2;
         x2 = xr;
-    } while( (std::abs(dxr) > epsilon * xr) && (iter < maxIterations) );
 
-    cerr << __FILE__ << ':' << __LINE__ << ": number of iterations = "
-         << iter << '\n';
+//        cerr << __FILE__ << ':' << __LINE__ << '\n';
+//        cerr << "\tden = " << den << '\n';
+//        cerr << "\tdxr = " << dxr << '\n';
+//        cerr << "\txr = " << xr << '\n';
+//        cerr << "\tx0 = " << x0 << '\n';
+//        cerr << "\tx1 = " << x1 << '\n';
+//        cerr << "\tx2 = " << x2 << '\n';
+    } while( (std::abs(dxr) > epsilon * xr) && (iterations < maxIterations) );
+
+//    cerr << __FILE__ << ':' << __LINE__ << ": number of iterations = "
+//         << iterations << '\n';
     
     return xr;
 }

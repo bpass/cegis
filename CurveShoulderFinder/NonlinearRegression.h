dee0ac5aedb687a17e99e0b23d1adc3e22b0023b
ahartman <ahartman>
2005-06-07 00:37:42 +0000

    A functor to do general nonlinear regression given a set of points and a function and its partial derivatives.

---
+++ CurveShoulderFinder/NonlinearRegression.h
@@ -0,0 +1,46 @@
+#ifndef AUSTIN_NONLINEARREGRESSION_H
+#define AUSTIN_NONLINEARREGRESSION_H
+
+#include <vector>
+#include "Point2D.h"
+//#include "Matrix.h"
+#include "DenseMatrix.h"
+
+template<class T>
+class NonlinearRegression
+{
+public:
+    typedef std::vector<Point2D<T> > Points;
+    typedef std::vector<T> Parameters;
+    typedef T (*Function)(const Parameters& p, const T& x);
+    typedef T (*FirstPartial)(const Parameters& p, const T& x);
+    typedef bool (*StoppingTest)(const Parameters& parameterChanges);
+    typedef std::vector<FirstPartial> FirstPartials;
+
+    Parameters operator()(const Points& points, 
+                          const Parameters& initialGuesses,
+                          const Function& f, 
+                          const FirstPartials& partials,
+                          const StoppingTest& stoppingTest,
+                          const size_t maxIterations = m_maxIterations) const;
+
+    class DifferentParametersPartialsSizes
+    {};
+
+    class NotEnoughPoints
+    {};
+
+private:
+    DenseMatrix<T> createZMatrix(const Points& points, 
+                                 const FirstPartials& partials,
+                                 const Parameters& parameters) const;
+    MyVector<T> createDVector(const Points& points, 
+                              const Function& f,
+                              const Parameters& parameters) const;
+    static const size_t m_maxIterations;
+};
+
+#include "NonlinearRegression.hpp"
+
+#endif
+
---
+++ CurveShoulderFinder/NonlinearRegression.hpp
@@ -0,0 +1,114 @@
+#include <cmath>
+#include "GaussianSolver.h"
+#include "PartialPivotingGaussianSolver.h"
+
+template<class T>
+const size_t NonlinearRegression<T>::m_maxIterations = 100;
+
+template<class T>
+typename NonlinearRegression<T>::Parameters
+NonlinearRegression<T>::
+operator()(const typename NonlinearRegression<T>::Points& points, 
+           const typename NonlinearRegression<T>::Parameters& initialGuesses,
+           const typename NonlinearRegression<T>::Function& f, 
+           const typename NonlinearRegression<T>::FirstPartials& partials,
+           const typename NonlinearRegression<T>::StoppingTest& stoppingTest,
+           const size_t maxIterations) 
+const
+{
+    using std::cout;
+
+    if(initialGuesses.size() != partials.size())
+    {
+        throw typename NonlinearRegression<T>::
+                       DifferentParametersPartialsSizes();
+    }
+
+    if(points.size() < partials.size())
+    {
+        throw typename NonlinearRegression<T>::NotEnoughPoints();
+    }
+
+    typename NonlinearRegression<T>::Parameters parameters(initialGuesses);
+    bool done = false;
+    size_t iterations = 0;
+
+    while(iterations < maxIterations && !done)
+    {
+        const DenseMatrix<T> Z = createZMatrix(points, partials, parameters);
+        cout << __FILE__ << ':' << __LINE__ << ':' << __func__ << ": "
+             << "Z[" << iterations << "] =\n" << Z;
+        
+        const DenseMatrix<T> ZT = Z.transpose();
+        cout << __FILE__ << ':' << __LINE__ << ':' << __func__ << ": "
+             << "ZT[" << iterations << "] =\n" << ZT;
+        
+        const DenseMatrix<T> ZTZ = ZT*Z;
+        cout << __FILE__ << ':' << __LINE__ << ':' << __func__ << ": "
+             << "ZT*Z[" << iterations << "] =\n" << ZTZ;
+        
+        const MyVector<T> D = createDVector(points, f, parameters);
+        cout << __FILE__ << ':' << __LINE__ << ':' << __func__ << ": "
+             << "D[" << iterations << "] =\n" << DenseMatrix<T>(D);
+        
+        const MyVector<T> ZTD = ZT*D;
+        cout << __FILE__ << ':' << __LINE__ << ':' << __func__ << ": "
+             << "ZT*D[" << iterations << "] =\n" << DenseMatrix<T>(ZTD);
+
+        const PartialPivotingGaussianSolver<T> gaussianSolver;
+        const typename GaussianSolver<T>::Solution solution = 
+            gaussianSolver(ZTZ, ZTD);
+        cout << __FILE__ << ':' << __LINE__ << ':' << __func__ << ": "
+             << "solution.matrix() =\n" << solution.matrix()
+             << "solution.vector() =\n" << DenseMatrix<T>(solution.vector());
+
+        typename NonlinearRegression<T>::Parameters
+            deltaA(solution.vector().getSize());
+        for(size_t i = 0; i < parameters.size(); ++i)
+        {
+            deltaA[i] = solution.vector()[i];
+            parameters[i] += deltaA[i];
+        }
+
+        done = stoppingTest(deltaA);
+        ++iterations;
+    }
+
+    return parameters;
+}
+
+template<class T>
+DenseMatrix<T>
+NonlinearRegression<T>::
+createZMatrix(const typename NonlinearRegression<T>::Points& points, 
+              const typename NonlinearRegression<T>::FirstPartials& partials,
+              const typename NonlinearRegression<T>::Parameters& parameters)
+const
+{
+    DenseMatrix<T> Z(points.size(), partials.size());
+    for(size_t j = 0; j < Z.getNumCols(); ++j)
+    {
+        for(size_t i = 0; i < Z.getNumRows(); ++i)
+        {
+            Z[i][j] = partials[j](parameters, points[i].x());
+        }
+    }
+    return Z;
+}
+
+template<class T>
+MyVector<T>
+NonlinearRegression<T>::
+createDVector(const typename NonlinearRegression<T>::Points& points,
+              const typename NonlinearRegression<T>::Function& f,
+              const typename NonlinearRegression<T>::Parameters& parameters) 
+const
+{
+    MyVector<T> D(points.size());
+    for(size_t i = 0; i < D.getSize(); ++i)
+    {
+        D[i] = points[i].y() - f(parameters, points[i].x());
+    }
+    return D;
+}
+

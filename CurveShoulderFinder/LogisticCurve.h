202bd8526bea7d66459f089c414f9bc85a449f95
ahartman <ahartman>
2005-06-07 00:42:53 +0000

    Modified to use more parameters so that the curve would fit better to the data points.  This required changing to use nonlinear regression.

--- CurveShoulderFinder/LogisticCurve.h
+++ CurveShoulderFinder/LogisticCurve.h
@@ -2,25 +2,43 @@
 #define AUSTIN_LOGISTICCURVE_H
 
 #include "FittingCurve.h"
+#include "MyVector.h"
+#include "DenseMatrix.h"
+#include "NonlinearRegression.h"
 
 template<class T>
 class LogisticCurve : public FittingCurve<T>
 {
 public:
-    LogisticCurve();
-    LogisticCurve(const typename FittingCurve<T>::Points& points, const T& L = 1);
+    LogisticCurve(const typename FittingCurve<T>::Points& points, 
+                  const T& aGuess, const T& bGuess, const T& cGuess);
 
     virtual void setPoints(const typename FittingCurve<T>::Points& points);
-    virtual void setL(const T& L);
+    virtual void setInitialGuesses(const T& aGuess, 
+                                   const T& bGuess, 
+                                   const T& cGuess);
     virtual T operator()(const T& x) const;
 
     virtual ~LogisticCurve();
 
 private:
-    T m_c, m_a;
-    T m_L;
+    T m_a, m_b, m_c;
+    static const size_t numParameters;
     typename FittingCurve<T>::Points m_points;
     void findSolution();
+    static T logisticFunction(const typename NonlinearRegression<T>::Parameters& p,
+                              const T& x);
+    static T firstPartialA(const typename NonlinearRegression<T>::Parameters& p,
+                           const T& x);
+    static T firstPartialB(const typename NonlinearRegression<T>::Parameters& p,
+                           const T& x);
+    static T firstPartialC(const typename NonlinearRegression<T>::Parameters& p,
+                           const T& x);
+    static T commonPartialDivisor(const typename NonlinearRegression<T>::
+                                                 Parameters& p,
+                                  const T& x);
+    static bool stoppingCondition(const typename NonlinearRegression<T>::
+                                                 Parameters& deltaA);
 };
 
 #include "LogisticCurve.hpp"
--- CurveShoulderFinder/LogisticCurve.hpp
+++ CurveShoulderFinder/LogisticCurve.hpp
@@ -1,14 +1,15 @@
 #include <cmath>
-#include "LinearRegression.h"
+#include "GaussianSolver.h"
+#include "PartialPivotingGaussianSolver.h"
 
 template<class T>
-LogisticCurve<T>::LogisticCurve()
-{}
+const size_t LogisticCurve<T>::numParameters = 3; 
 
 template<class T>
 LogisticCurve<T>::
-LogisticCurve(const typename FittingCurve<T>::Points& points, const T& L)
-    : m_L(L), m_points(points)
+LogisticCurve<T>(const typename FittingCurve<T>::Points& points, 
+                 const T& a, const T& b, const T& c)
+    : m_a(a), m_b(b), m_c(c), m_points(points)
 {
     findSolution();
 }
@@ -27,9 +28,11 @@ LogisticCurve<T>::setPoints(const typename FittingCurve<T>::Points& points)
 
 template<class T>
 void
-LogisticCurve<T>::setL(const T& L)
+LogisticCurve<T>::setInitialGuesses(const T& a, const T& b, const T& c)
 {
-    m_L = L;
+    m_a = a;
+    m_b = b;
+    m_c = c;
     findSolution();
 }
 
@@ -37,26 +40,107 @@ template<class T>
 T
 LogisticCurve<T>::operator()(const T& x) const
 {
-    return m_L / (1 + m_c * std::exp(m_a * x));
+    typename NonlinearRegression<T>::Parameters p;
+    p.reserve(numParameters);
+    p.push_back(m_a);
+    p.push_back(m_b);
+    p.push_back(m_c);
+    return logisticFunction(p, x);
+//    return m_a/(1+m_b*std::exp(-m_c*x));
 }
 
 template<class T>
 void
 LogisticCurve<T>::findSolution()
 {
-    typename FittingCurve<T>::Points transformedPoints;
-    transformedPoints.reserve(m_points.size());
+    using std::cout;
 
-    for(typename FittingCurve<T>::Points::iterator i = m_points.begin();
-        i != m_points.end(); ++i)
-    {
-        transformedPoints.push_back(Point2D<T>(i->x(), 
-                                               std::log(m_L/i->y() - 1)));
-    }
+    typename NonlinearRegression<T>::Parameters initialGuesses;
+    initialGuesses.reserve(numParameters);
+    initialGuesses.push_back(m_a);
+    initialGuesses.push_back(m_b);
+    initialGuesses.push_back(m_c);
 
-    LinearRegression<T> lr;
-    typename LinearRegression<T>::Solution lrSolution = lr(transformedPoints);
+    typename NonlinearRegression<T>::FirstPartials partials;
+    partials.reserve(numParameters);
+    partials.push_back(firstPartialA);
+    partials.push_back(firstPartialB);
+    partials.push_back(firstPartialC);
 
-    m_c = std::exp(lrSolution.a0());
-    m_a = lrSolution.a1();
+    NonlinearRegression<T> nlr;
+    typename NonlinearRegression<T>::Parameters parameters =
+        nlr(m_points, initialGuesses, 
+            logisticFunction, partials, stoppingCondition);
+    m_a = parameters[0];
+    m_b = parameters[1];
+    m_c = parameters[2];
+
+    cout << __FILE__ << ':' << __LINE__ << ':' << __func__ << ":\n"
+         << "\ta = " << m_a << '\n'
+         << "\tb = " << m_b << '\n'
+         << "\tc = " << m_c << '\n';
 }
+
+template<class T>
+T
+LogisticCurve<T>::logisticFunction(const typename NonlinearRegression<T>::
+                                                  Parameters& p,
+                                   const T& x)
+{
+    return p[0]/(1+p[1]*std::exp(-p[2]*x));
+}
+
+template<class T>
+T
+LogisticCurve<T>::firstPartialA(const typename NonlinearRegression<T>::
+                                               Parameters& p,
+                                const T& x)
+{
+    return 1/(commonPartialDivisor(p, x));
+}
+
+template<class T>
+T
+LogisticCurve<T>::firstPartialB(const typename NonlinearRegression<T>::
+                                               Parameters& p,
+                                const T& x)
+{
+    const T cpd = commonPartialDivisor(p, x);
+    return -p[0]*std::exp(-p[2]*x)/(cpd*cpd);
+}
+
+template<class T>
+T
+LogisticCurve<T>::firstPartialC(const typename NonlinearRegression<T>::
+                                               Parameters& p,
+                                const T& x)
+{
+    const T cpd = commonPartialDivisor(p, x);
+    return p[0]*p[1]*x*std::exp(-p[2]*x)/(cpd*cpd);
+}
+
+template<class T>
+T
+LogisticCurve<T>::commonPartialDivisor(const typename NonlinearRegression<T>::
+                                                      Parameters& p,
+                                       const T& x)
+{
+    return 1+p[1]*std::exp(-p[2]*x);
+}
+
+template<class T>
+bool
+LogisticCurve<T>::stoppingCondition(const typename NonlinearRegression<T>::
+                                                   Parameters& deltaA)
+{
+    const T epsilon = .0001;
+    for(size_t i = 0; i < deltaA.size(); ++i)
+    {
+        if(std::abs(deltaA[i]) > epsilon)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+

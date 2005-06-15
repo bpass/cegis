c43e42c07cdbcf9836e979d483d112dac5188047
ahartman <ahartman>
2005-06-15 01:53:30 +0000

    Wrapped the output inside several preprocessor directives so that it could easily be turned on and off.
    Changed to use a second input file that should contain the initial guesses for the parameters of the curve.  This was done to eliminate the need to recompile every time the initial guesses changed.

--- CurveShoulderFinder/driver.cpp
+++ CurveShoulderFinder/driver.cpp
@@ -2,7 +2,7 @@
  * @file driver.cpp
  * @author Austin Hartman
  *
- * $Id: driver.cpp,v 1.3 2005/06/14 23:47:54 ahartman Exp $
+ * $Id: driver.cpp,v 1.4 2005/06/15 01:53:30 ahartman Exp $
  */
 
 #include <cmath>
@@ -18,6 +18,10 @@
 #include "CurvePartitioner.h"
 
 using namespace std;
+
+//#define PRINT_BOUNDS
+#define PRINT_VALUES
+//#define PRINT_DIFFERENCES
 
 template<class T>
 void printValues(const size_t n, const T& lowValue, const T& highValue, 
@@ -56,44 +60,40 @@ int main(int argc, char* argv[])
 	// set up some typedefs for easier reading/modification
 	typedef double type;
 
-    // set up the output options
-    cout.precision(4);
-    cout << fixed;
-
     // open the input file, after first making sure one was specified
-	if(argc <= 1)
+	if(argc <= 2)
 	{
-		cerr << argv[0] << ':' << " no input file\n";
+		cerr << argv[0] << ':' 
+             << " need a points file and an initial guesses file\n";
 		return 1;
 	}
-	ifstream inputFile(argv[1]);
+
+    // read in the points
+	ifstream pointsInputFile(argv[1]);
 
 	// read in the number of pairs of x and y coordinates
 	size_t numPoints;
-	inputFile >> numPoints;
-
-	// check to see if the number of pairs is valid
-	if(numPoints < 2)
-	{
-		cerr << "The number of pairs is too small to find an "
-		        "interpolating polynomial\n";
-		return 2;
-	}
-    
-    // read in the points
+	pointsInputFile >> numPoints;
     CubicSplineCurve<type>::Points points(numPoints);
     for(size_t i = 0; i < numPoints; ++i)
     {
-        inputFile >> points[i];
+        pointsInputFile >> points[i];
     }
+
+    // read in the initial guesses
+    ifstream initialGuessesFile(argv[2]);
+    type aGuess, bGuess, cGuess;
+    initialGuessesFile >> aGuess >> bGuess >> cGuess;
 
     // create the fitting curve
     CubicSplineCurve<type> splineCurve(points);
 //    LogisticCurve<type> logisticCurve(points, 9.81, 1.03E-5, -2.28);
-    LogisticCurve<type> logisticCurve(points, 10, .1, -2);
+//    LogisticCurve<type> logisticCurve(points, 9.7, .0001, -2.2);
+//    LogisticCurve<type> logisticCurve(points, 10, .1, -2);
 //    LogisticCurve<type> logisticCurve(points, 10, 1, -1);
-//    FittingCurve<type>& curve = logisticCurve;
-    FittingCurve<type>& curve = splineCurve;
+    LogisticCurve<type> logisticCurve(points, aGuess, bGuess, cGuess);
+    FittingCurve<type>& curve = logisticCurve;
+//    FittingCurve<type>& curve = splineCurve;
 
     // get the differences between successive y values along the curve
     CurvePartitioner<type> partitioner;
@@ -111,27 +111,35 @@ int main(int argc, char* argv[])
         partitioner.getDifferences(curve, numberOfPartitions, 
                                    lowerBound, upperBound);
 
-//    const size_t width = cout.precision() + 4;
+    // set up the output options
+    cout.precision(12);
+    cout << fixed;
+    const size_t width = cout.precision() + 4;
 
+#ifdef PRINT_BOUNDS
     // print out some values at various points
-//    cout << "lowerBound = " << lowerBound << '\n';
-//    cout << "upperBound = " << upperBound << '\n';
-//    cout << "L = " << logisticCurveL << '\n';
+    cout << "lowerBound = " << lowerBound << '\n';
+    cout << "upperBound = " << upperBound << '\n';
+#endif
 
-//    const type range = upperBound - lowerBound;
-//    for(size_t i = 0; i <= numberOfPartitions; ++i)
-//    {
-//        const type currentX = lowerBound + (range / numberOfPartitions) * i;
-//        cout << currentX << ", " << curve(currentX) << '\n';
-//    }
+#ifdef PRINT_VALUES
+    const type range = upperBound - lowerBound;
+    for(size_t i = 0; i <= numberOfPartitions; ++i)
+    {
+        const type currentX = lowerBound + (range / numberOfPartitions) * i;
+        cout << currentX << ", " << curve(currentX) << '\n';
+    }
 
 //    printValues(numberOfPartitions, lowerBound, upperBound, curve);
+#endif
 
-//    for(size_t i = 0; i < differences.size(); ++i)
-//    {
-//        cout << "differences[" << setw(3) << i << "] = " 
-//             << setw(width) << std::abs(differences[i]) << '\n';
-//    }
+#ifdef PRINT_DIFFERENCES
+    for(size_t i = 0; i < differences.size(); ++i)
+    {
+        cout << "differences[" << setw(3) << i << "] = " 
+             << setw(width) << std::abs(differences[i]) << '\n';
+    }
+#endif
     cout << '\n';
 }
 

508621b5fcfcb781efbd0ce88b68ba14ec55ade1
mswilliams <mswilliams>
2005-06-16 23:38:45 +0000

    Added Goode's Homolosine projection

---
+++ GCTP_CPP/goodeh.h
@@ -0,0 +1,24 @@
+
+#ifndef GOODE_H
+#define GOODE_H
+#include "pseudocylindrical.h"
+
+class GoodeH : public Pseudocylindrical
+{
+public:
+
+	GoodeH();
+	GoodeH(double gctpParams[], ProjUnit units, ProjDatum dat);
+
+private:
+
+	double m_centerLons[12];
+	double m_falseEastings[12];
+
+	void init();
+
+	void _forward(double lon, double lat);
+	void _inverse(double x, double y);
+};
+
+#endif

c304541fdde14ca7847fe097bbb3ff814bf29439
mswilliams <mswilliams>
2005-06-24 01:49:56 +0000

    Added Lambert azimuthal

---
+++ GCTP_CPP/lambertazimuth.h
@@ -0,0 +1,24 @@
+#ifndef LAMBERT_AZ_H
+#define LAMBERT_AZ_H
+
+#include "azimuthal.h"
+
+class LambertAzimuthal : public Azimuthal
+{
+public:
+	LambertAzimuthal();
+	LambertAzimuthal(double gctpParams[], ProjUnit units, ProjDatum dat);
+
+protected:
+
+	double m_sinCenterLat;
+	double m_cosCenterLat;
+
+	void init();
+
+	void _forward(double lon, double lat);
+	void _inverse(double x, double y);
+
+};
+
+#endif

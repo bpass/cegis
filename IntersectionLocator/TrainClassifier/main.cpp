4eb4be04a064cee2a5753c791c0774cb678c6a99
ahartman <ahartman>
2005-07-21 01:03:22 +0000

    Updated to train the classifier for the foristelli data.
    Added a function to output a classified image as a GeoTiff, so that it contains georeferencing and projection information, and thus can easily be compared to the original image by using, e.g. Imagine's Link Viewers function.

--- IntersectionLocator/TrainClassifier/main.cpp
+++ IntersectionLocator/TrainClassifier/main.cpp
@@ -15,24 +15,21 @@ using namespace std;
 
 void outputClassifiedImage( std::string ortho, std::string outputFile,
                             Classifier &C );
+void outputClassifiedImageAsGeoTiff( std::string ortho, std::string outputFile,
+                                     Classifier &C );
 
 int main()
 {
+    using std::cerr;
+    using std::cout;
+
     const std::string rd("D:/Data/foristell_final/training/images/roads/c2_r7.tif");
     const std::string nonrd("D:/Data/foristell_final/training/images/nonroads/c2_r7.tif");
     const std::string outfile("D:/Data/foristell_final/training/output.dat");
-//    string rd( "F:\\rstelzleni\\GA2Quads\\classifierTraining\\"
-//               "fewroads515605.tif" );
-//    string nonrd( "F:\\rstelzleni\\GA2Quads\\classifierTraining\\"
-//                  "nonroads515605.tif" );
-//    string outfile( "F:\\rstelzleni\\GA2Quads\\"
-//                    "classifierTraining\\newTrainingTest.dat" );
-//    string testOrtho( "F:\\rstelzleni\\GA2Quads\\"
-//                "classifierTraining\\TestImage\\16SGC515605.tif" );
-//    string testOutput( "F:\\rstelzleni\\GA2Quads\\"
-//                "classifierTraining\\TestImage\\newTrySeven.dat" );
+    const std::string testOrtho("D:/Data/foristell_final/orthoimages/c2_r7.tif");
+    const std::string testOutput("D:/Data/foristell_final/training/output/c2_r7.tif");
 
-    cout << "Running\n";
+    cerr << "Running\n";
 
     GDALAllRegister();
     GDALDataset *pRd, *pNonRd;
@@ -52,14 +49,15 @@ int main()
 
     // Comment or uncomment lines as you see fit
     Classifier C;
-    C.inputText( outfile.c_str() );
+    //C.inputText( outfile.c_str() );
     //for( int i=0; i<5; ++i )
     //   C.addRdTraining( pRd );
     C.addRdTraining(pRd);
     C.addNonRdTraining( pNonRd );
     C.outputText( outfile.c_str() );
 
-    //outputClassifiedImage( testOrtho, testOutput, C );
+//    outputClassifiedImage( testOrtho, testOutput, C );
+    outputClassifiedImageAsGeoTiff( testOrtho, testOutput, C );
 
     return 0;
 }
@@ -76,7 +74,7 @@ void outputClassifiedImage( std::string ortho, std::string outputFile,
     image = static_cast<GDALDataset *>( GDALOpen( ortho.c_str(), GA_ReadOnly ) );
     if( image == NULL )
     {
-        fprintf( stderr, "Failed to open image file for test run\n" );
+        cerr << "Failed to open image file for test run\n";
         return;
     }
 
@@ -88,4 +86,124 @@ void outputClassifiedImage( std::string ortho, std::string outputFile,
     return;
 }
 
+// take the image named ortho, classify it with C and put the result
+// in a generic binary file called outputFile.  For instructions on
+// viewing the output, see the doxygen documentation for the main
+// project, on the page called Preparing for new data.
+void outputClassifiedImageAsGeoTiff( std::string ortho, std::string outputFile,
+                                     Classifier &C )
+{
+    using std::cerr;
+    const unsigned char isRoadValue = 255;
+    const unsigned char isNonRoadValue = 0;
+
+    GDALDataset *image;
+    image = static_cast<GDALDataset *>( GDALOpen( ortho.c_str(), GA_ReadOnly ) );
+    if( image == NULL )
+    {
+        cerr << "Failed to open image file for test run\n";
+        return;
+    }
+
+    cerr << "Getting GeoTiff driver\n";
+    GDALDriver* tiffDriver = GetGDALDriverManager()->GetDriverByName("GTiff");
+    if( tiffDriver == NULL)
+    {
+        cerr << "Failed to get GeoTiff driver\n";
+        return;
+    }
+
+    cerr << "Creating a new output image\n";
+    GDALDataset* outputImage = 
+        tiffDriver->Create( outputFile.c_str(), 
+                            image->GetRasterXSize(), image->GetRasterYSize(),
+                            1, GDT_Byte, NULL );
+    if( outputImage == NULL )
+    {
+        cerr << "Failed to create a new output image\n";
+        return;
+    }
+
+    cerr << "Getting the projection of the input image\n";
+    const std::string imageProjection = image->GetProjectionRef();
+    if( imageProjection != "" )
+    {
+        cerr << "Setting the projection of the output image to "
+             << imageProjection << "\n";
+        const CPLErr setProjectionError = 
+            outputImage->SetProjection( imageProjection.c_str() );
+
+        if( setProjectionError == CE_Failure )
+        {
+            cerr << "An error occurred when setting the projection\n";
+        }
+    }
+    else
+    {
+        cerr << "Input image had no projection information\n";
+    }
+
+    cerr << "Getting the GeoTransform of the input image\n";
+    double imageGeoTransform[6];
+    const CPLErr getGeoTransformError =
+        image->GetGeoTransform( imageGeoTransform );
+    if( getGeoTransformError == CE_Failure )
+    {
+        cerr << "Failed getting the geo transform of the input image\n";
+    }
+
+    cerr << "Setting the GeoTransform of the output image\n";
+    const CPLErr setGeoTransformError =
+        outputImage->SetGeoTransform( imageGeoTransform );
+    if( setGeoTransformError == CE_Failure )
+    {
+        cerr << "Failed setting the geo transform of the output image\n";
+    }
+
+    cerr << "Copying the input image to an InMemRaster\n";
+    InMemRaster rasta( image );
+    cerr << "Changing the InMemRaster to HSV\n";
+    rasta.convertToHSV();
+    cerr << "Classifying the InMemRaster\n";
+    rasta.classify( C );
+
+    GDALRasterBand* band = outputImage->GetRasterBand( 1 );
+    for(int j = 0; j < rasta.getYSize(); ++j)
+    {
+//        if( (j+1) % 100 == 0 )
+//        {
+//            cerr << "Writing line " << j+1 << " of the output image\n";
+//        }
+        for(int i = 0; i < rasta.getXSize(); ++i)
+        {
+            if( rasta.isRoad( i, j ) )
+            {
+                band->RasterIO( GF_Write, 
+                                i, j,
+                                1, 1,
+                                &(const_cast<unsigned char&>(isRoadValue)),
+                                1, 1,
+                                GDT_Byte,
+                                0, 0);
+            }
+            else
+            {
+                band->RasterIO( GF_Write, 
+                                i, j,
+                                1, 1,
+                                &(const_cast<unsigned char&>(isNonRoadValue)),
+                                1, 1,
+                                GDT_Byte,
+                                0, 0);
+            }
+        }
+    }
+    
+    cerr << "Closing output image\n";
+    delete outputImage; outputImage = NULL;
+
+//    rasta.dumpToBinary( outputFile.c_str() );
+
+    return;
+}
 

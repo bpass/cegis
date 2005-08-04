c81fc4ad072040825e4aec81ed77e063387c10ca
ahartman <ahartman>
2005-08-04 22:34:27 +0000

    Updated to use the new example bsq and header files.
    Changed the epsilon in the float comparison function to take into account that Imagine only displays 3 digits past the decimal.

--- CurveShoulderFinder/tests/BSQReader_t.cpp
+++ CurveShoulderFinder/tests/BSQReader_t.cpp
@@ -2,7 +2,7 @@
  * @file BSQReader_t.cpp
  * @author Austin Hartman
  *
- * $Id: BSQReader_t.cpp,v 1.2 2005/07/07 18:58:55 ahartman Exp $
+ * $Id: BSQReader_t.cpp,v 1.3 2005/08/04 22:34:27 ahartman Exp $
  */
 
 #include "BSQReader.h"
@@ -25,9 +25,9 @@ int main()
 #endif
 
     const std::string bsqFileName = 
-        "./littler30_nitro.bsq";
+        "./1920newstack.bsq";
     const std::string headerFileName = 
-        "./littler30_nitro.hdr";
+        "./1920newstack.hdr";
     typedef float type;
 
     // create two BSQReaders using the two different constructors
@@ -35,17 +35,17 @@ int main()
     BSQReader<type> reader2(bsqFileName, headerFileName);
 
     // check if the information read from the header file is correct
-    const size_t expectedNumBands = 6;
-    const size_t expectedNumRows = 1274;
-    const size_t expectedNumCols = 1155;
+    const size_t expectedNumBands = 15;
+    const size_t expectedNumRows = 20;
+    const size_t expectedNumCols = 19;
 
     const BSQReader<type>::UTMCoordinateType expectedMinX = 231405.5;
-    const BSQReader<type>::UTMCoordinateType expectedMaxX = 266025.5;
-    const BSQReader<type>::UTMCoordinateType expectedMinY = 3482011.5;
+    const BSQReader<type>::UTMCoordinateType expectedMaxX = 265965.5;
+    const BSQReader<type>::UTMCoordinateType expectedMinY = 3483721.5;
     const BSQReader<type>::UTMCoordinateType expectedMaxY = 3520201.5;
     
-    const BSQReader<type>::UTMCoordinateType expectedPixelWidth = 30.0;
-    const BSQReader<type>::UTMCoordinateType expectedPixelHeight = 30.0;
+    const BSQReader<type>::UTMCoordinateType expectedPixelWidth = 1920.0;
+    const BSQReader<type>::UTMCoordinateType expectedPixelHeight = 1920.0;
 
     assert(reader.getNumBands() == expectedNumBands);
     assert(reader.getNumRows() == expectedNumRows);
@@ -71,88 +71,88 @@ int main()
     // check some values in the bsq file to see if they are reported correctly
     // ==== Point 0 ====
     const BSQReader<type>::UTMCoordinateType point0X = expectedMinX;
-    const BSQReader<type>::UTMCoordinateType point0Y = expectedMaxY;
+    const BSQReader<type>::UTMCoordinateType point0Y = expectedMinY;
     const type point0Band0Value = 0.000;
-    const type point0Band3Value = 0.000;
+    const type point0Band2Value = 64.000;
 
 #ifdef PRINT_VALUES
     cerr << "reader.getValue(point0X, point0Y, 0) = " 
          << reader.getValue(point0X, point0Y, 0) << '\n';
-    cerr << "reader.getValue(point0X, point0Y, 3) = " 
-         << reader.getValue(point0X, point0Y, 3) << '\n';
+    cerr << "reader.getValue(point0X, point0Y, 2) = " 
+         << reader.getValue(point0X, point0Y, 2) << '\n';
 #endif
     assert(floatsEqual(reader.getValue(point0X, point0Y, 0), point0Band0Value));
-    assert(floatsEqual(reader.getValue(point0X, point0Y, 3), point0Band3Value));
+    assert(floatsEqual(reader.getValue(point0X, point0Y, 2), point0Band2Value));
     assert(floatsEqual(reader2.getValue(point0X, point0Y, 0), point0Band0Value));
-    assert(floatsEqual(reader2.getValue(point0X, point0Y, 3), point0Band3Value));
+    assert(floatsEqual(reader2.getValue(point0X, point0Y, 2), point0Band2Value));
 
     // ==== Point 1 ====
     const BSQReader<type>::UTMCoordinateType point1X = expectedMinX;
     const BSQReader<type>::UTMCoordinateType point1Y = expectedMaxY;
     const type point1Band0Value = 0.000;
-    const type point1Band3Value = 0.000;
+    const type point1Band2Value = 32.000;
 
 #ifdef PRINT_VALUES
     cerr << "reader.getValue(point1X, point1Y, 0) = " 
          << reader.getValue(point1X, point1Y, 0) << '\n';
-    cerr << "reader.getValue(point1X, point1Y, 3) = " 
-         << reader.getValue(point1X, point1Y, 3) << '\n';
+    cerr << "reader.getValue(point1X, point1Y, 2) = " 
+         << reader.getValue(point1X, point1Y, 2) << '\n';
 #endif
     assert(floatsEqual(reader.getValue(point1X, point1Y, 0), point1Band0Value));
-    assert(floatsEqual(reader.getValue(point1X, point1Y, 3), point1Band3Value));
+    assert(floatsEqual(reader.getValue(point1X, point1Y, 2), point1Band2Value));
     assert(floatsEqual(reader2.getValue(point1X, point1Y, 0), point1Band0Value));
-    assert(floatsEqual(reader2.getValue(point1X, point1Y, 3), point1Band3Value));
+    assert(floatsEqual(reader2.getValue(point1X, point1Y, 2), point1Band2Value));
 
     // ==== Point 2 ====
-    const BSQReader<type>::UTMCoordinateType point2X = 239533.389454;
-    const BSQReader<type>::UTMCoordinateType point2Y = 3503942.107063; 
-    const type point2Band0Value = 0.220;
-    const type point2Band3Value = 1.220;
-    const type point2Band5Value = 0.290;
+    const BSQReader<type>::UTMCoordinateType point2X = 250391.940678;
+    const BSQReader<type>::UTMCoordinateType point2Y = 3497456.415254; 
+    const type point2Band0Value = 77.000;
+    const type point2Band3Value = 79.000;
+    const type point2Band12Value = 3.000;
 
 #ifdef PRINT_VALUES
     cerr << "reader.getValue(point2X, point2Y, 0) = " 
          << reader.getValue(point2X, point2Y, 0) << '\n';
     cerr << "reader.getValue(point2X, point2Y, 3) = " 
          << reader.getValue(point2X, point2Y, 3) << '\n';
-    cerr << "reader.getValue(point2X, point2Y, 5) = " 
-         << reader.getValue(point2X, point2Y, 5) << '\n';
+    cerr << "reader.getValue(point2X, point2Y, 12) = " 
+         << reader.getValue(point2X, point2Y, 12) << '\n';
 #endif
     assert(floatsEqual(reader.getValue(point2X, point2Y, 0), point2Band0Value));
     assert(floatsEqual(reader.getValue(point2X, point2Y, 3), point2Band3Value));
-    assert(floatsEqual(reader.getValue(point2X, point2Y, 5), point2Band5Value));
+    assert(floatsEqual(reader.getValue(point2X, point2Y, 12), point2Band12Value));
     assert(floatsEqual(reader2.getValue(point2X, point2Y, 0), point2Band0Value));
     assert(floatsEqual(reader2.getValue(point2X, point2Y, 3), point2Band3Value));
-    assert(floatsEqual(reader2.getValue(point2X, point2Y, 5), point2Band5Value));
+    assert(floatsEqual(reader2.getValue(point2X, point2Y, 12), point2Band12Value));
 
-    // ==== Point 2 ====
+    // ==== Point 3 ====
     const BSQReader<type>::UTMCoordinateType point3X = 243676.347701;
     const BSQReader<type>::UTMCoordinateType point3Y = 3500067.099730;
-    const type point3Band0Value = 11019.810;
-    const type point3Band3Value = 0.590;
-    const type point3Band5Value = 0.000;
+    const type point3Band0Value = 57.000;
+    const type point3Band4Value = 0.177;
+    const type point3Band14Value = 1.000;
 
 #ifdef PRINT_VALUES
     cerr << "reader.getValue(point3X, point3Y, 0) = " 
          << reader.getValue(point3X, point3Y, 0) << '\n';
-    cerr << "reader.getValue(point3X, point3Y, 3) = " 
-         << reader.getValue(point3X, point3Y, 3) << '\n';
-    cerr << "reader.getValue(point3X, point3Y, 5) = " 
-         << reader.getValue(point3X, point3Y, 5) << '\n';
+    cerr << "reader.getValue(point3X, point3Y, 4) = " 
+         << reader.getValue(point3X, point3Y, 4) << '\n';
+    cerr << "reader.getValue(point3X, point3Y, 14) = " 
+         << reader.getValue(point3X, point3Y, 14) << '\n';
 #endif
     assert(floatsEqual(reader.getValue(point3X, point3Y, 0), point3Band0Value));
-    assert(floatsEqual(reader.getValue(point3X, point3Y, 3), point3Band3Value));
-    assert(floatsEqual(reader.getValue(point3X, point3Y, 5), point3Band5Value));
+    assert(floatsEqual(reader.getValue(point3X, point3Y, 4), point3Band4Value));
+    assert(floatsEqual(reader.getValue(point3X, point3Y, 14), point3Band14Value));
     assert(floatsEqual(reader2.getValue(point3X, point3Y, 0), point3Band0Value));
-    assert(floatsEqual(reader2.getValue(point3X, point3Y, 3), point3Band3Value));
-    assert(floatsEqual(reader2.getValue(point3X, point3Y, 5), point3Band5Value));
+    assert(floatsEqual(reader2.getValue(point3X, point3Y, 4), point3Band4Value));
+    assert(floatsEqual(reader2.getValue(point3X, point3Y, 14), point3Band14Value));
 }
 
 template<class T>
 bool
 floatsEqual(T v1, T v2)
 {
-    const T epsilon = .00001;
+    const T epsilon = .001;
     if(std::abs(v1 - v2) > epsilon)
     {
         return false;

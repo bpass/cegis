15e958892df0c608cf40a9b8f1db1794ad58071d
ahartman <ahartman>
2005-08-22 19:05:25 +0000

    An interface base class for reading from raster files.  The eventual plan is to read  from img files instead of bsq files (since that requires conversion using Imagine,  plus more disk space), so I went ahead and abstracted the ability to get values from raster files when giving UTM coordinates.

---
+++ CurveShoulderFinder/RasterReader.h
@@ -0,0 +1,110 @@
+/**
+ * @file RasterReader.h
+ * @author Austin Hartman
+ *
+ * $Id: RasterReader.h,v 1.1 2005/08/22 19:05:25 ahartman Exp $
+ */
+
+#ifndef AUSTIN_RASTERREADER_H
+#define AUSTIN_RASTERREADER_H
+
+/**
+ * An interface base class for the user to access values from raster
+ * files when they give UTM coordinates.
+ */
+template<class DataType>
+class RasterReader
+{
+public:
+    /**
+     * Used to indicate when we are referring to types that are UTM 
+     * coordinates.
+     */
+    typedef double UTMCoordinateType;
+
+    /**
+     * Pure virtual destructor.
+     */
+    virtual ~RasterReader() = 0;
+
+    /**
+     * Get the value from the raster in the specified band at the 
+     * specified UTM coordinates.
+     *
+     * @param xCoord The x-coordinate of the location we want to get the
+     * value from.
+     * @param yCoord The y-coordinate of the location we want to get the
+     * value from.
+     * @param band The band we want to get the value from.  Valid bands are 
+     * from 1 to getNumBands().
+     *
+     * @return The value from the image at the specified position and band.
+     */
+    virtual DataType getValue(const UTMCoordinateType& xCoord, 
+                              const UTMCoordinateType& yCoord,
+                              size_t band) const = 0;
+
+    /**
+     * Gets the minimum value of the x-coordinate that the image contains
+     * data for.
+     *
+     * @return The minimum x-coordinate.
+     */
+    virtual UTMCoordinateType getMinX() const = 0;
+
+    /**
+     * Gets the maximum value of the x-coordinate that the image contains
+     * data for.
+     *
+     * @return The maximum x-coordinate.
+     */
+    virtual UTMCoordinateType getMaxX() const = 0;
+
+    /**
+     * Gets the minimum value of the y-coordinate that the image contains
+     * data for.
+     *
+     * @return The minimum y-coordinate.
+     */
+    virtual UTMCoordinateType getMinY() const = 0;
+
+    /**
+     * Gets the maximum value of the y-coordinate that the image contains
+     * data for.
+     *
+     * @return The maximum y-coordinate.
+     */
+    virtual UTMCoordinateType getMaxY() const = 0;
+    
+    /**
+     * Gets the number of bands in the image.
+     *
+     * @return The number of bands.
+     */
+    virtual size_t getNumBands() const = 0;
+
+    /**
+     * Exception class to be used when the user requests data from an
+     * x-coordinate that the source image does not contain.
+     */
+    class InvalidXCoordinate
+    {};
+
+    /**
+     * Exception class to be used when the user requests data from an
+     * y-coordinate that the source image does not contain.
+     */
+    class InvalidYCoordinate
+    {};
+
+    /**
+     * Exception class to be used when the user requests data from a band not
+     * contained in the source image.
+     */
+    class InvalidBand
+    {};
+};
+
+#include "RasterReader.hpp"
+
+#endif
---
+++ CurveShoulderFinder/RasterReader.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file RasterReader.hpp
+ * @author Austin Hartman
+ *
+ * $Id: RasterReader.hpp,v 1.1 2005/08/22 19:05:25 ahartman Exp $
+ */
+
+#ifdef AUSTIN_RASTERREADER_H
+#ifndef AUSTIN_RASTERREADER_HPP
+#define AUSTIN_RASTERREADER_HPP
+
+template<class DataType>
+RasterReader<DataType>::~RasterReader()
+{}
+
+#endif
+#endif
+

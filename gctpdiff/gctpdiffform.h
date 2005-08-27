523c0385c536f8d6f27ea59d3160035d88174c98
rbuehler <rbuehler>
2005-08-27 00:30:44 +0000

    Removed an unneccesary main.cpp from the gctpcpp and updated the .pro to reflect that. Started creation of the GctpDiffForm.

--- gctpdiff/gctpdiffform.h
+++ gctpdiff/gctpdiffform.h
@@ -1,2 +1,27 @@
-// $Id: gctpdiffform.h,v 1.1 2005/08/27 00:20:32 rbuehler Exp $
+// $Id: gctpdiffform.h,v 1.2 2005/08/27 00:30:44 rbuehler Exp $
 
+
+#include <QDialog>
+
+class QVBoxLayout;
+class QPushButton;
+class RasterDropAcceptor;
+class QLabel;
+
+class GctpDiffForm : public QDialog
+{
+   Q_OBJECT
+
+public:
+   GctpDiffForm();
+
+   QVBoxLayout *mainLayout;
+
+   QPushButton *diffButton;
+
+   RasterDropAcceptor *inputAcceptor;
+   QLabel *inputLabel;
+
+   RasterDropAcceptor *outputAcceptor;
+   QLabel *outputLabel;
+}

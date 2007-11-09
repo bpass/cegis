e4c4d97d38b420c2c06974424bec9736221d175e
dmattli <dmattli>
2007-11-09 21:39:24 +0000

    Added forward and inverse tests...

--- mapimg/src/projection/unit-tests/mercator-test.cpp
+++ mapimg/src/projection/unit-tests/mercator-test.cpp
@@ -18,7 +18,7 @@ using namespace trans;
 
 #define MAJ 6378206.4 // Clarke 1866 ellipsoid
 #define MIN 6356583.8
-#define DIFF 50.0
+#define TOL 3  // maximum percent difference
 
 void constructors_test ()
 {
@@ -27,13 +27,31 @@ void constructors_test ()
 
 void forward_test ()
 {
-  mercator<double> m1(MAJ, MIN); 
+  mercator<long double> m1(MAJ, MIN); 
   
-  BOOST_CHECK_CLOSE(m1.forward_y(0.0, 0.0)/MAJ, 1e-20, 1e10);
-  //BOOST_CHECK_EQUAL(m1.forward_y(50, 50), 3773);
+  // Test forward_y
+  BOOST_CHECK_CLOSE(m1.forward_y(0.0, 0.0)/MAJ+1, 1.0L, TOL);
+  BOOST_CHECK_CLOSE(m1.forward_y(0.0872664626,0.0872664626)/MAJ, 0.08679, TOL); // 5 deg
+  BOOST_CHECK_CLOSE(m1.forward_y(0.174532925,0.174532925)/MAJ, 0.17425, TOL); // 10 deg
+  BOOST_CHECK_CLOSE(m1.forward_y(0.261799388, 0.26179938)/MAJ, 0.26309, TOL); // 15 deg
 
+  // Test forward_x
+  BOOST_CHECK_CLOSE(m1.forward_x(0.0, 0.0)/MAJ+1, 1.0, TOL);
+  BOOST_CHECK_CLOSE(m1.forward_x(0.0872664626, 0.0872664626)/MAJ, 0.017453*5, TOL);
+  BOOST_CHECK_CLOSE(m1.forward_x(0.174532925,0.174532925)/MAJ, 0.017453*10, TOL);
+  BOOST_CHECK_CLOSE(m1.forward_x(0.261799388, 0.26179938)/MAJ, 0.017453*15, TOL);
+
+  return;
 }
 
+void inverse_test ()
+{
+  mercator<double> m1(MAJ, MIN);
+  
+  BOOST_CHECK_CLOSE(m1.inverse_lon(0.0,0.0)+1.0, 1.0, TOL);
+
+  return;
+}
 
 test_suite*
 init_unit_test_suite( int argc, char* argv[] )
@@ -42,6 +60,7 @@ init_unit_test_suite( int argc, char* argv[] )
 
   test->add( BOOST_TEST_CASE( &constructors_test ));
   test->add( BOOST_TEST_CASE( &forward_test ));
+  test->add( BOOST_TEST_CASE( &inverse_test ));
   
   return test;
 }
